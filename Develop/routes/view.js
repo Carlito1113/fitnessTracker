3b644add5f908d2d4d7fc5074eef64680444e833
Carlito Algarin <carlito1113@yahoo.com>
2020-10-29 19:31:31 -0400

    added in variables to require path and express. also made an module.exports function for express and created the first route for the root

--- Develop/routes/view.js
+++ Develop/routes/view.js
@@ -1,1 +1,12 @@
-// Serve up HTML files here
+const express = require("express");
+const path = require("path");
+const app = express();
+
+module.exports = app => {
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    
+
+}
