199372c0a89584303ed365f69aeaa66bf42da2b1
Carlito Algarin <carlito1113@yahoo.com>
2020-10-30 17:25:20 -0400

    added in the workout.js schema object

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,40 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const workoutSchema = new Schema();
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercise: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter and exercise"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise name"
+                },
+                duration: {
+                    type: Number,
+                    required: "In minutes, enter the duration of the exercise."
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    }
+);
