ee6e8f4405b8792991b04fbc1d926213d254b953
Carlito Algarin <carlito1113@yahoo.com>
2020-10-30 17:30:10 -0400

    dynamically-created property for schema in workout.js

--- models/workout.js
+++ models/workout.js
@@ -1,40 +1,55 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const workoutSchema = new Schema(
-    {
-        day: {
-            type: Date,
-            default: () => new Date()
+  {
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
         },
-        exercise: [
-            {
-                type: {
-                    type: String,
-                    trim: true,
-                    required: "Enter and exercise"
-                },
-                name: {
-                    type: String,
-                    trim: true,
-                    required: "Enter an exercise name"
-                },
-                duration: {
-                    type: Number,
-                    required: "In minutes, enter the duration of the exercise."
-                },
-                weight: {
-                    type: Number
-                },
-                reps: {
-                    type: Number
-                },
-                sets: {
-                    type: Number
-                },
-                distance: {
-                    type: Number
-                }
-            }
-        ]
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      // include any virtual properties when data is requested
+      virtuals: true
     }
+  }
 );
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+  // "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
